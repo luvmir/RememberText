a6e25291f6e3034e41e15d97bf95400413dffe08
ALEX YANG <alex82.yang@gmail.com>
2018-04-17 12:54:42 +0900

    Add a feature that load vocabulary list
    able to load multiple vocabulary files (should be defined in
    /res/voca_list.csv)
    change vocabulary file type from "/" sperated to "," sperated
    (CSV)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
 $(document).ready(function(){
-	var DEFINE_INPUT_FILE_NAME = "./res/data.txt";
-	var DEFINE_DELIMETER = "/";
+	var DEFINE_VOCA_LIST_FILE_NAME = "./res/voca_list.csv"
+	var DEFINE_CSV_DELIMETER = ",";
+	var DEFINE_VOCA_DIR_PATH = "./res/";
 
+	var currentVocaFileName = "";
+	var vocaList = [];
 	var questionList = [];
 	var totalCount = 0;
 	var currentIndex = 0;
 
-	$("#input_korean:text").val("");
-	$("#input_english:text").val("");
+    function increaseIndex() {
+    	if (currentIndex == totalCount - 1) {
+    		currentIndex = 0;
+    	} else {
+    		currentIndex++;
+    	}
+    }
 
-	$("#button_check").hide();
-	$("#button_pass").hide();
+    function decreaseIndex() {
+    	if (currentIndex <= 0) {
+    		currentIndex = totalCount - 1;
+    	} else {
+    		currentIndex--;
+    	}
+    }
+
+	function initScreen() {
+		$("#div_container_question").hide();
+		$("#button_start").hide();
+		$("#button_check").hide();
+		$("#button_pass").hide();
+		$("#button_prev").hide();
+		$("#button_next").hide();
+		eraseAllInputField();
+	}
+
+	function updateScreenAfterVocaLoaded() {
+		$("#div_container_question").show();
+		$("#button_load_voca_list").hide();
+		$("#button_start").show();
+	}
+
+	function updateScreenAfterStarted() {
+		$("#button_check").show();
+		$("#button_pass").show();
+		$("#button_prev").show();
+		$("#button_next").show();
+	}
+
+	function eraseAllInputField() {
+		$("#input_korean:text").val("");
+		$("#input_english:text").val("");
+	}
 
 	function checkMatched(savedString, typedString) {
 		if (savedString == typedString) {
@@ -20,6 +61,10 @@ $(document).ready(function(){
 	}
 
 	function updateQuestion() {
+		if ( currentIndex == -1 ) {
+			currentIndex = totalCount - 1;
+		}
+
 	    if ( currentIndex == totalCount ) {
 			currentIndex = 0;
 		}
@@ -27,17 +72,27 @@ $(document).ready(function(){
 		$("#div_index_info").html((currentIndex+1) + " / " + totalCount);
 		$("#input_korean:text").val(questionList[currentIndex].korean);
 	    $("#input_hidden_english:text").val(questionList[currentIndex].english);
+	}
 
-	    currentIndex++;
+	function loadVocaList() {
+		var vocaListText = $("#div_hidden_voca_list").text();
+		var lines = vocaListText.split("\n");
+
+		for (var i = 0; i < lines.length; i++) {
+			var innerLine = lines[i].split(DEFINE_CSV_DELIMETER);
+			var voca = { "description" : innerLine[0], "fileName" : innerLine[1] };
+			vocaList.push (voca);
+		}
+
+		return lines.length;
 	}
 
     function parsingContents() {
-    	var contents = $("#div_hidden_full_contents").text()
-
+    	var contents = $("#div_hidden_full_contents").text();
         var lines = contents.split("\n");
 
 		for (var i = 0; i < lines.length; i++) {
-			var innerLine = lines[i].split(DEFINE_DELIMETER);
+			var innerLine = lines[i].split(DEFINE_CSV_DELIMETER);
 			var question = { "korean" : innerLine[0], "english" : innerLine[1] };
 			questionList.push (question);
 		}
@@ -45,23 +100,50 @@ $(document).ready(function(){
 		return lines.length;
     }
 
-    $("#div_hidden_full_contents").load(DEFINE_INPUT_FILE_NAME);
-    
-    $("#button_start").click(function(){
+    function addVocaList() {
+    	for (var i = 0; i < vocaList.length; i++) {
+    		$('#select_voca_list').append($('<option>', { 
+		        value: vocaList[i].fileName,
+		        text : vocaList[i].description 
+		    }));
+    	}
+    }
+
+    $("#select_voca_list").change(function() {
+    	if ($("#select_voca_list").val() != "default") {
+    		currentVocaFileName = $("#select_voca_list").val();
+    		$("#div_hidden_full_contents").load(DEFINE_VOCA_DIR_PATH + currentVocaFileName, function(responseTxt, statusTxt, xhr){
+        		if(statusTxt == "success") {
+    				updateScreenAfterVocaLoaded();
+        		}
+        		if(statusTxt == "error") {
+            		alert("Voca loading error! (" + xhr.status + ":" + xhr.statusText + ")");
+            		initScreen();
+            	}
+    		});
+    	}
+    });
+
+    $("#button_load_voca_list").click(function() {
+    	loadVocaList();
+    	addVocaList();
+    });
+
+
+    $("#button_start").click(function() {
 
     	if($("#button_start").text() == "Start!") {
     		totalCount = parsingContents();
-    		$("#button_start").hide();
-    		$("#button_check").show();
-    		$("#button_pass").show();
     		updateQuestion();
+    		updateScreenAfterStarted();
     	}
    	
     });
 
-    $("#button_check").click(function(){
+    $("#button_check").click(function() {
     	if (checkMatched($("#input_hidden_english").val(), $("#input_english").val())) {
     		alert("Matched!");
+    		increaseIndex();
     		updateQuestion();
     		$("#input_english:text").val("");
     	} else {
@@ -69,9 +151,24 @@ $(document).ready(function(){
     	}   	
     });
 
-    $("#button_pass").click(function(){
+    $("#button_pass").click(function() {
     	alert($("#input_hidden_english").val());
+    	increaseIndex();
    		updateQuestion();
     });
 
+    $("#button_prev").click(function() {
+    	decreaseIndex();
+   		updateQuestion();
+    });
+
+    $("#button_next").click(function() {
+    	increaseIndex();
+   		updateQuestion();
+    });
+
+    
+    $("#div_hidden_voca_list").load(DEFINE_VOCA_LIST_FILE_NAME);
+    
+    initScreen();
 });	
