8864c0a489fe9ebe9fd5325470de413e8381e421
ALEX YANG <alex82.yang@gmail.com>
2018-04-17 13:14:15 +0900

    Erase all input field before updating question

--- js/main.js
+++ js/main.js
@@ -69,6 +69,7 @@ $(document).ready(function(){
 			currentIndex = 0;
 		}
 
+		eraseAllInputField():
 		$("#div_index_info").html((currentIndex+1) + " / " + totalCount);
 		$("#input_korean:text").val(questionList[currentIndex].korean);
 	    $("#input_hidden_english:text").val(questionList[currentIndex].english);
@@ -167,7 +168,7 @@ $(document).ready(function(){
    		updateQuestion();
     });
 
-    
+
     $("#div_hidden_voca_list").load(DEFINE_VOCA_LIST_FILE_NAME);
     
     initScreen();
